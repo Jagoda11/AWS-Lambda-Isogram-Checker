fae071d3d66581b393979394781aa987cfca7348
Jagoda11 <jagoda.cubrilo@yahoo.com>
2023-04-25 18:03:12 +0200

    added serverless aws lambda

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const checker = require('../isogram');
+const checker = require('./isogram');
 
-module.exports.handler = (event, context, callback) => {
+module.exports.isIsogramHandler = (event) => {
   const word = event.queryStringParameters.word;
   const isIsogram = checker.isIsogram(word);
 
-  callback(null, {
+  return {
     statusCode: 200,
     body: JSON.stringify({ isIsogram }),
-  });
+  };
 };
